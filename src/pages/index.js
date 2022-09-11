ddd83174db5d31895794cfbaffc5e51061d732d3
kwing25 <kendracwing@icloud.com>
2022-09-11 14:48:40 -0600

    Working on site content and build...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import * as React from "react";
 
-import BlockContent from "../components/block-content";
-
 const pageStyles = {
   color: "#232129",
   padding: 96,
@@ -128,7 +126,6 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <BlockContent />
       <h1 style={headingStyles}>
         Congratulations
         <br />
