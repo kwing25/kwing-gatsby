0307b6ae1b14c7360eb630d8a987cd07d1b1ad25
kwing25 <kendracwing@icloud.com>
2022-09-17 15:14:05 -0600

    Installing/updating packages/files...

--- public/component---src-pages-index-js-f1a54599ff5bf8e58443.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkkwing_gatsby=self.webpackChunkkwing_gatsby||[]).push([[678],{6558:function(e,t,n){n.r(t),n.d(t,{Head:function(){return s}});var a=n(7294),r={color:"#232129",padding:96,fontFamily:"-apple-system, Roboto, sans-serif, serif"};t.default=function(){return a.createElement("main",{style:r},a.createElement("h1",{className:"text-purple-500"},"IndexPage"))};var s=function(){return a.createElement("title",null,"Home Page")}}}]);
-//# sourceMappingURL=component---src-pages-index-js-f1a54599ff5bf8e58443.js.map
--- src/components/blog/blog-post-preview.jsx
+++ src/components/blog/blog-post-preview.jsx
@@ -12,7 +12,7 @@ function BlogPostPreview(props) {
                 <img className="absolute inset-0 object-cover w-full h-full" src={imageUrlFor(buildImageObj(props.mainImage))} alt={props.mainImage.alt} />
             )}
 
-            <div className="relative p-8 bg-black/30 transition duration-0 hover:duration-150 hover:bg-black rounded-xl">
+            <div className="relative rounded-xl">
                 <div id="blog-preview-card-title" className="rounded-2xl mb-1">
                     <h3 className="goo my-2">{props.title}</h3>
                 </div>
@@ -22,7 +22,7 @@ function BlogPostPreview(props) {
                         {props.categories && props.categories.length > 0 && (
                             <div className="inline-flex">
                                 {props.categories.map(category => (
-                                    <div key={category._id} className="flex px-2 mx-2 mt-2 rounded-lg  bg-[#ff006e]/50 hover:bg-[#ff006e]/80">
+                                    <div key={category._id} className="flex px-2 mx-2 mt-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-tag-fill mr-1" viewBox="0 0 16 16">
                                             <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
                                             <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
