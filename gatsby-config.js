cbdfe855f0b1a7147fb055b1a1e8893d7ab3a09d
kwing25 <kendracwing@icloud.com>
2022-11-29 14:55:53 -0700

    Working on website...
    Updating site and changing files. In progress...

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 // Load variables from `.env` as soon as possible
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV || "development"}`,
-});
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 const clientConfig = require("./client-config");
 
--- src/components/blog/blog-post-preview-grid.jsx
+++ src/components/blog/blog-post-preview-grid.jsx
@@ -1,38 +1,19 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby"
+
 //import BlogPostPreview from "./blog-post-preview";
 
 function BlogPostPreviewGrid() {
 
-    //     const data = useStaticQuery(graphql`
-    //     query {
-    //       sanityBlog {
-    //         title
-    //         slug {
-    //           current
-    //         }
-    //         _rawExcerpt
-    //       }
-    //     }
-    //   `
-    //     )
+
 
 
     return (
         <div>
             <h1>Blog Post Preview Grid</h1>
 
-            {/* {data.sanityBlog.title} */}
 
 
             <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-                {/* {props.nodes &&
-                props.nodes.map((node) => (
-                    <div key={node.id} className="overflow-hidden rounded-2xl h-full text-center">
-                        <BlogPostPreview {...node} />
-                    </div>
-                ))} */}
-
 
             </div>
         </div>
